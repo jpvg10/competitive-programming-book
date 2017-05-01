ac055897889a7a73f715fb0d20fe76e12d08e258
jpvg95 <juan.valencia10a@gmail.com>
2017-05-01 13:54:26 -0500

    Dijkstra

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,76 +1,77 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+
+class Nodo implements Comparable<Nodo>{
+	int id;
+	int distancia;
+	public Nodo(int id, int distancia){
+		this.id = id;
+		this.distancia = distancia;
+	}	
+	public int compareTo(Nodo o) {
+		return this.distancia-o.distancia;
+	}
+}
 
 public class Dijkstra {
 	
 	static ArrayList<Integer> g[];
-	static Nodo[] dist;
-	static PriorityQueue<Nodo> proximo;
-	static int[] parent;
 	static int[][] p;
-	
+	static Nodo[] nodos;
+	static int[] padre;
+	static boolean[] visited;
+	static PriorityQueue<Nodo> proximo;	
+		
 	public static void main(String[] args) throws IOException {
-		int nodos = 5;
+		int n = 8;
 		
-		g = new ArrayList[nodos];
-		dist = new Nodo[nodos];
-		parent = new int[nodos];
+		g = new ArrayList[n];
+		p = new int[n][n];
+		nodos = new Nodo[n];
+		padre = new int[n];
+		visited = new boolean[n];
 		proximo = new PriorityQueue<Nodo>();
-		p = new int[nodos][nodos];
-		
-		for (int i = 0; i < nodos; i++) {
+				
+		for (int i=0; i<n; i++) {
 			g[i] = new ArrayList<Integer>();
-			dist[i] = new Nodo(i, Integer.MAX_VALUE);				
+			nodos[i] = new Nodo(i, Integer.MAX_VALUE);				
 		}
 		
-		int src = 0;
-		int dest = 4;
-
-		dist[src].peso = 0;
-		proximo.add(dist[src]);
-		parent[src] = src;
-		
-		Nodo a;
+		//Aca empieza Dijkstra. Llenar antes 'g' y 'p' (lsita y matriz de adyacencia)
+		int src = 0;		
+		nodos[src].distancia = 0;
+		proximo.add(nodos[src]);
+		padre[src] = src;
+				
 		while(!proximo.isEmpty()) {
-			a = proximo.poll();
-			calcularDistancia(a);
+			Nodo u = proximo.poll();
+			if(!visited[u.id]){
+				visited[u.id] = true;
+				int len = g[u.id].size();
+				for (int i=0; i<len; i++) {
+					int v = g[u.id].get(i);
+					if(u.distancia + p[u.id][v] < nodos[v].distancia){
+						nodos[v].distancia = u.distancia + p[u.id][v];
+						proximo.add(nodos[v]);
+						padre[v] = u.id;
+					}
+				}
+			}			
 		}
-
-		int mejorDistancia = dist[dest].peso;
 		
-		int r = dest;
-		//bw.write(r+"\n");
-		while(r != src){
-			r = parent[r];
-			//bw.write(r+"\n");
+		//El vector 'nodos' contiene la menor distancia de 'src' a todos los nodos
+		//Por ejemplo, la menor distancia de 'src' a 4 es
+		int menorDist = nodos[4].distancia;
+		
+		//Para hallar el camino como tal entre 'src' y un nodo
+		LinkedList<Integer> camino = new LinkedList<Integer>();
+		int r = 4;		
+		camino.add(r);
+		while(r != src){			
+			r = padre[r];
+			camino.addFirst(r);
 		}
-		//bw.flush();
-	}
-	
-	public static void calcularDistancia(Nodo u){
-		int t = g[u.n].size();
-		for (int i = 0; i < t; i++) {
-			int x = g[u.n].get(i);
-			if(u.peso + p[u.n][x] < dist[x].peso){
-				dist[x].peso = u.peso + p[u.n][x];
-				proximo.add(dist[x]);
-				parent[x] = u.n;
-			}
-		}
-	}
-}
-
-class Nodo implements Comparable<Nodo>{
-	int n;
-	int peso;
-
-	public Nodo(int n, int peso){
-		this.n = n;
-		this.peso = peso;
-	}
-	
-	public int compareTo(Nodo o) {
-		return this.peso-o.peso;
 	}
 }
