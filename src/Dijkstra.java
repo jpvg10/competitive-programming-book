f5ee78d86525e2dee90bd803db323cd589e3055b
jpvg95 <juan.valencia10a@gmail.com>
2017-05-04 16:56:48 -0500

    Dijkstra

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -9,46 +9,62 @@ class Nodo implements Comparable<Nodo>{
 	public Nodo(int id, int distancia){
 		this.id = id;
 		this.distancia = distancia;
-	}	
+	}
 	public int compareTo(Nodo o) {
 		return this.distancia-o.distancia;
 	}
 }
 
 public class Dijkstra {
-	
+
 	static ArrayList<Integer> g[];
 	static int[][] p;
 	static Nodo[] nodos;
 	static int[] padre;
-	static boolean[] visited;
-	static PriorityQueue<Nodo> proximo;	
-		
+	static boolean[] visitado;
+	static PriorityQueue<Nodo> proximo;
+
 	public static void main(String[] args) throws IOException {
 		int n = 8;
-		
+
 		g = new ArrayList[n];
 		p = new int[n][n];
 		nodos = new Nodo[n];
 		padre = new int[n];
-		visited = new boolean[n];
+		visitado = new boolean[n];
 		proximo = new PriorityQueue<Nodo>();
-				
+
 		for (int i=0; i<n; i++) {
 			g[i] = new ArrayList<Integer>();
-			nodos[i] = new Nodo(i, Integer.MAX_VALUE);				
+			nodos[i] = new Nodo(i, Integer.MAX_VALUE);
 		}
-		
-		//Aca empieza Dijkstra. Llenar antes 'g' y 'p' (lsita y matriz de adyacencia)
-		int src = 0;		
+
+		int src = 0;
+		dijkstra(src);
+
+		//El vector 'nodos' contiene la menor distancia de 'src' a todos los nodos
+		//Por ejemplo, la menor distancia de 'src' a 4 es:
+		int menorDist = nodos[4].distancia;
+
+		//Para hallar el camino como tal entre 'src' y un nodo
+		LinkedList<Integer> camino = new LinkedList<Integer>();
+		int r = 4;
+		camino.add(r);
+		while(r != src){
+			r = padre[r];
+			camino.addFirst(r);
+		}
+	}
+
+	public static void dijkstra(int src){
 		nodos[src].distancia = 0;
 		proximo.add(nodos[src]);
 		padre[src] = src;
-				
+
 		while(!proximo.isEmpty()) {
 			Nodo u = proximo.poll();
-			if(!visited[u.id]){
-				visited[u.id] = true;
+			if(!visitado[u.id]){
+				visitado[u.id] = true;
 				int len = g[u.id].size();
 				for (int i=0; i<len; i++) {
 					int v = g[u.id].get(i);
@@ -58,20 +74,7 @@ public class Dijkstra {
 						padre[v] = u.id;
 					}
 				}
-			}			
-		}
-		
-		//El vector 'nodos' contiene la menor distancia de 'src' a todos los nodos
-		//Por ejemplo, la menor distancia de 'src' a 4 es
-		int menorDist = nodos[4].distancia;
-		
-		//Para hallar el camino como tal entre 'src' y un nodo
-		LinkedList<Integer> camino = new LinkedList<Integer>();
-		int r = 4;		
-		camino.add(r);
-		while(r != src){			
-			r = padre[r];
-			camino.addFirst(r);
+			}
 		}
 	}
 }
