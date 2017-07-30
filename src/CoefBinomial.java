0da774d0e71a1e79d89d242f36b59b7e5cf51062
jpvg95 <juan.valencia10a@gmail.com>
2017-07-29 19:00:23 -0500

    More combinatorics

---
+++ src/CoefBinomial.java
@@ -0,0 +1,16 @@
+
+public class CoefBinomial {
+
+	public static void main(String[] args) {
+		int n = 10;
+		int[][] C = new int[n][n];
+		
+		for(int i=0; i<n; i++){
+			C[i][0] = 1;
+			for(int j=1; j<=i; j++){
+				C[i][j] = C[i-1][j-1] + C[i-1][j];
+			}
+		}
+	}
+
+}
