23698a560ff44676b7cfd80f3ce061689331dac7
jpvg95 <juan.valencia10a@gmail.com>
2017-10-24 15:29:30 -0500

    Small fix in binomial coefficients

--- src/CoefBinomial.java
+++ src/CoefBinomial.java
@@ -1,16 +1,16 @@
-
 public class CoefBinomial {
-
-	public static void main(String[] args) {
-		int n = 10;
-		long[][] C = new long[n][n];
+	
+	public static long[][] binomial(int n) {
+		long[][] C = new long[n+1][n+1];
 		
-		for(int i=0; i<n; i++){
+		for(int i=0; i<=n; i++){
 			C[i][0] = 1;
 			for(int j=1; j<=i; j++){
 				C[i][j] = C[i-1][j-1] + C[i-1][j];
 			}
 		}
+		
+		return C;
 	}
 
 }
